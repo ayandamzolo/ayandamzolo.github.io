bb93bcd5a1e04491b78d60a440f714ac2528cbac
ayandamzolo <135819703+ayandamzolo@users.noreply.github.com>
2024-02-10 15:55:04 +0200

    Update send_email.php

--- send_email.php
+++ send_email.php
@@ -6,12 +6,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $message = $_POST['message'];
   $to = 'ayandamzolo11@gmail.com';
   $headers = "From: $name <$email>";
+
   if (mail($to, $subject, $message, $headers)) {
     http_response_code(200);
-    exit;
+    exit("Message sent successfully!");
   } else {
     http_response_code(500);
-    exit;
+    exit("Failed to send message. Please try again later.");
   }
+} else {
+  http_response_code(405);
+  exit("Method Not Allowed");
 }
 ?>
